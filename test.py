9e64e9656a2f80cf78f95893595f9857bd6dcb94
Shregar1 <sengarsinghshreyansh@gmail.com>
2022-01-04 23:56:03 +0530

    added training teating configs

--- test.py
+++ test.py
@@ -3,10 +3,11 @@ import torch
 from src.models.generator import Generator
 from src.testing.tester import Tester
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 test_dir = "test"
 outputs_dir = "outputs/results/testing"
 weights_dir = "weights"
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 G_filename = "Generator_model_perceptual_real_color_70_512_9_.pth"
 G_path = os.path.join(weights_dir,G_filename)
 G = Generator()
--- train.py
+++ train.py
@@ -5,14 +5,11 @@ from src.models.discriminator import Discriminator
 from src.training.trainer import Trainer
 
 train_stage = 1
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dataset_dir = "dataset"
 outputs_dir = "outputs/results/training"
 weights_dir = "weights"
 G_filename = "Generator_model_perceptual_real_color_70_512_9_.pth"
 D_finename = "Discriminator_model_perceptual_real_color_70_512_9_.pth"
-G_path = os.path.join(weights_dir,G_filename)
-D_path = os.path.join(weights_dir,D_finename)
 lambda_L1 = 100
 lambda_Per = 1
 lambda_Col = 50
@@ -21,6 +18,10 @@ lambda_Sty = 1
 lambda_HSV = 1 
 lambda_YUV = 1
 num_epochs = 1
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+G_path = os.path.join(weights_dir,G_filename)
+D_path = os.path.join(weights_dir,D_finename)
 G = Generator()
 D = Discriminator()
 G.to(device)
@@ -28,7 +29,7 @@ D.to(device)
 G = torch.load(G_path)
 D = torch.load(D_path)
 
-train=Trainer(device=device, dataset_dir=dataset_dir, outputs_dir=outputs_dir, 
+train = Trainer(device=device, dataset_dir=dataset_dir, outputs_dir=outputs_dir, 
               G=G, D=D, lambda_L1=lambda_L1, lambda_Per=lambda_Per, 
               lambda_Col=lambda_Col, lambda_Con=lambda_Con, lambda_Sty=lambda_Sty, 
               lambda_HSV=lambda_HSV, lambda_YUV=lambda_YUV)

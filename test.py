47d2668df90047c3ba9c8c9740c45ba424b1535a
Shregar1 <sengarsinghshreyansh@gmail.com>
2022-01-06 01:47:38 +0530

    tesing script fix

--- test.py
+++ test.py
@@ -3,16 +3,18 @@ import torch
 from src.models.generator import Generator
 from src.testing.tester import Tester
 
+
 test_dir = "test"
 outputs_dir = "outputs/results/testing"
 weights_dir = "weights"
+G_filename = "Generator_weights_anime_sketch_2_color.pth"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-G_filename = "Generator_model_perceptual_real_color_70_512_9_.pth"
 G_path = os.path.join(weights_dir,G_filename)
 G = Generator()
 G.to(device)
-G = torch.load(G_path)
+G.load_state_dict(torch.load(G_path))
 
-test = Tester(G=G, test_dir=test_dir, outputs_dir=outputs_dir)
+
+test = Tester(G=G, test_dir=test_dir, outputs_dir=outputs_dir, device=device)
 test.fit()
